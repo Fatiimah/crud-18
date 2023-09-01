edf760bef628a5f0fa04541c38f3879afad1e923
Fatiimah <fatimah.wo@mail.wu.ac.th>
2023-09-01 20:40:08 +0700

    Initial commit

---
+++ lib/screens/config.dart
@@ -0,0 +1,11 @@
+import 'package:thelast/screens/users.dart';
+
+class Configure{
+  static const server = "10.116.1.82:3000";
+  static Users login = Users();
+  static List<String> gender = [
+    "None",
+    "Male",
+    "Female",
+  ];
+}
